b8321150d11b5762794e8aff68e8c81dded158c2
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-07-12 19:56:59 -0700

    feat: implement set-credentials

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Subsonic CLI
 ![Hackatime badge](https://hackatime-badge.hackclub.com/U07V1ND4H0Q/scli)
+CLI application to interact with a Subsonic server or a Subsonic API-compatible server
 
 To install dependencies:
 
--- src/index.ts
+++ src/index.ts
@@ -34,19 +34,40 @@ app
   .argument("<url>", "URL of server")
   .action((url: string) => {
     if (!isValidURL(url)) {
-      console.log('Error - invalid URL specified');
+      console.log("Error - invalid URL specified");
       return;
     }
 
-    const configPath = path.join(import.meta.dirname, '../config.yml');
+    const configPath = path.join(import.meta.dirname, "../config.yml");
     if (existsSync(configPath)) {
-      const yamlData = yaml.load(readFileSync(configPath, 'utf8'));
+      const yamlData = yaml.load(readFileSync(configPath, "utf8"));
       (yamlData as ConfigFile).url = url;
       writeFileSync(configPath, yaml.dump(yamlData));
+      return;
     }
     writeFileSync(configPath, yaml.dump({ url }));
 
-    console.log(`Set server URL to ${url}`)
+    console.log(`Set server URL to ${url}`);
+  });
+
+app
+  .command("set-credentials")
+  .description("Set server login credentials")
+  .argument("<username>", "Server username")
+  .argument("<password>", "Server password")
+  .action((username: string, password: string) => {
+    const configPath = path.join(import.meta.dirname, "../config.yml");
+    if (existsSync(configPath)) {
+      const yamlData = yaml.load(readFileSync(configPath, "utf8"));
+      (yamlData as ConfigFile).username = username;
+      (yamlData as ConfigFile).password = password;
+
+      writeFileSync(configPath, yaml.dump(yamlData));
+      return;
+    }
+    writeFileSync(configPath, yaml.dump({ username, password }));
+
+    console.log(`Credentials saved successfully!`);
   });
 
 app.parse();
