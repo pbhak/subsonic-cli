aadb07c0c40419a30658d106491f692babac2fc1
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-07-12 20:50:45 -0700

    feat: implement server ping command

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import { Command } from "commander";
+import { md5 } from "js-md5";
 import * as yaml from "js-yaml";
+import { randomBytes } from "node:crypto";
 import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import path from "node:path";
 
-interface ConfigFile {
+type ConfigFile = {
   username?: string;
   password?: string;
   url?: string;
+}
+
+type PingResponse = {
+  "subsonic-response": {
+    status: string
+    version: string
+    type: string
+    serverVersion: string
+    openSubsonic: boolean
+  }
 }
 
 function isValidURL(url: string) {
@@ -20,6 +32,7 @@ function isValidURL(url: string) {
 }
 
 const app = new Command();
+const configPath = path.join(import.meta.dirname, "../config.yml");
 
 app
   .name("scli")
@@ -38,11 +51,22 @@ app
       return;
     }
 
-    const configPath = path.join(import.meta.dirname, "../config.yml");
     if (existsSync(configPath)) {
-      const yamlData = yaml.load(readFileSync(configPath, "utf8"));
-      (yamlData as ConfigFile).url = url;
+      const yamlData = yaml.load(
+        readFileSync(configPath, "utf8")
+      ) as ConfigFile;
+
+      if (!yamlData) {
+        writeFileSync(configPath, yaml.dump({ url }));
+
+        console.log(`Set server URL to ${url}`);
+        return;
+      }
+
+      yamlData.url = url;
+
       writeFileSync(configPath, yaml.dump(yamlData));
+      console.log(`Set server URL to ${url}`);
       return;
     }
     writeFileSync(configPath, yaml.dump({ url }));
@@ -56,18 +80,76 @@ app
   .argument("<username>", "Server username")
   .argument("<password>", "Server password")
   .action((username: string, password: string) => {
-    const configPath = path.join(import.meta.dirname, "../config.yml");
     if (existsSync(configPath)) {
-      const yamlData = yaml.load(readFileSync(configPath, "utf8"));
-      (yamlData as ConfigFile).username = username;
-      (yamlData as ConfigFile).password = password;
+      const yamlData = yaml.load(
+        readFileSync(configPath, "utf8")
+      ) as ConfigFile;
+
+      if (!yamlData) {
+        writeFileSync(configPath, yaml.dump({ username, password }));
+
+        console.log(`Credentials saved successfully!`);
+        return;
+      }
+
+      yamlData.username = username;
+      yamlData.password = password;
 
       writeFileSync(configPath, yaml.dump(yamlData));
+      console.log(`Credentials saved successfully!`);
       return;
     }
     writeFileSync(configPath, yaml.dump({ username, password }));
 
     console.log(`Credentials saved successfully!`);
+  });
+
+app
+  .command("ping")
+  .description("Check connection to server")
+  .action(async () => {
+    if (existsSync(configPath)) {
+      const yamlData = yaml.load(
+        readFileSync(configPath, "utf8")
+      ) as ConfigFile;
+
+      if (!yamlData) {
+        console.log("Error - invalid config");
+        return;
+      }
+
+      if (!yamlData.url) {
+        console.log("Error - no URL set");
+        return;
+      } else if (!yamlData.username || !yamlData.password) {
+        console.log("Error - credentials not set");
+        return;
+      }
+
+      const pingURL = new URL("/rest/ping", yamlData.url);
+      const salt = randomBytes(6).toString("hex");
+      const hash = md5(yamlData.password + salt);
+
+      pingURL.searchParams.set("u", yamlData.username);
+      pingURL.searchParams.set("t", hash);
+      pingURL.searchParams.set("s", salt);
+      pingURL.searchParams.set("v", "1.16.1");
+      pingURL.searchParams.set("c", "scli");
+      pingURL.searchParams.set("f", "json");
+
+      try {
+        const ping = await fetch(pingURL).then(async res => await res.json()) as PingResponse;
+        if (ping["subsonic-response"].status === 'ok') {
+          console.log(`Server ${pingURL.origin} is online!`)
+          return;
+        }
+      } catch (error) {
+        console.log(`An error occured while pinging ${pingURL.origin}`);
+        return;
+      } 
+    }
+
+    console.log("Error - URL and credentials not set");
   });
 
 app.parse();
